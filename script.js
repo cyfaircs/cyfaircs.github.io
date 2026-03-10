0aaa73d8507f10f8e72fa92a631204dbda22f81f
NateADL3 <nathandl01@yahoo.com>
2026-03-09 21:23:31 -0500

    Commented out theme button.

--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-/*
-
-// grab the ID for ease of use
-const tabStart = document.getElementById("tab-start");
-const tabProjects = document.getElementById("tab-projects");
-const tabMembers = document.getElementById("tab-members");
-
-// grab the pages for ease of use
-const pageStart = document.getElementById("page-start");
-const pageProjects = document.getElementById("page-projects");
-const pageMembers = document.getElementById("page-members");
-
-// dont know if we need these, just have them here in case
-const tabs = [tabStart, tabProjects, tabMembers];
-const pages = [pageStart, pageProjects, pageMembers]; 
-
-// event Listeners
-tabStart.addEventListener("click", function() {
-  window.location.href = "index.html"
-});
-
-tabProjects.addEventListener("click", function() {
-  window.location.href = "projects.html"
-});
-
-tabMembers.addEventListener("click", function() {
-  window.location.href = "members.html"
-});
-
-*/
-
-let currentBackgroundColor = "black";
-
 function toWhereToStart() {
   window.location.href = "wheretostart.html";
 };
@@ -55,6 +22,10 @@ function goBack() {
   }
 }
 
+/* Code for a theme button:
+
+let currentBackgroundColor = "black";
+
 function changeBackground() {
   if (currentBackgroundColor === "black") {
     document.body.style.background = "white";
@@ -64,4 +35,4 @@ function changeBackground() {
     document.body.style.background = "black";
     currentBackgroundColor = "black";
   }
-}
+} */
